08e6044f13eb728157310e3b1b3984f0042de3de
Sergey.Pak <sergey.pak@jetbrains.com>
2016-03-11 16:24:55 +0300

    Handle all kinds of exceptions that can occur while executing VmwareTask. Cherry-picked from master

---
+++ cloud-vmware-server/src/test/java/jetbrains/buildServer/clouds/vmware/connector/VmwareTaskWrapperTest.java
@@ -0,0 +1,54 @@
+package jetbrains.buildServer.clouds.vmware.connector;
+
+import com.vmware.vim25.InvalidProperty;
+import com.vmware.vim25.RuntimeFault;
+import com.vmware.vim25.TaskInfo;
+import com.vmware.vim25.mo.Task;
+import java.rmi.RemoteException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import jetbrains.buildServer.BaseTestCase;
+import jetbrains.buildServer.clouds.base.connector.CloudTaskResult;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Sergey.Pak on 3/11/2016.
+ */
+@Test
+public class VmwareTaskWrapperTest extends BaseTestCase {
+
+
+  public void should_report_isdone_for_error_tasks() throws ExecutionException, InterruptedException {
+    VmwareTaskWrapper taskWrapper = new VmwareTaskWrapper(new Callable<Task>() {
+      @Override
+      public Task call() throws Exception {
+        return new Task(null, null){
+          @Override
+          public TaskInfo getTaskInfo() throws InvalidProperty, RuntimeFault, RemoteException {
+            throw new RuntimeException("getTaskInfo exception");
+          }
+
+          @Override
+          public String waitForTask() throws RuntimeFault, RemoteException, InterruptedException {
+            throw new RuntimeException("getTaskInfo exception");
+          }
+
+          @Override
+          public void cancelTask() throws RuntimeFault, RemoteException {
+            throw new RuntimeException("getTaskInfo exception");
+          }
+        };
+      }
+    }, "myTask");
+    final Future<CloudTaskResult> async = taskWrapper.executeOrGetResultAsync();
+    int cnt = 0;
+    while (!async.isDone()){
+      cnt++;
+      assertTrue(cnt < 5);
+    }
+    final CloudTaskResult result = async.get();
+    assertTrue(result.isHasErrors());
+  }
+
+}
