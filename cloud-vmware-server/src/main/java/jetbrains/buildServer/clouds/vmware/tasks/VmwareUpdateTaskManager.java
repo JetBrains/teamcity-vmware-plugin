7e5db11cb360d64d2999794cb2d6c9ebfb411aa1
Sergey Pak <paksv@yandex.ru>
2017-12-21 15:52:42 +0100

    TW-53047 Deadlock between VmwarePooledUpdateInstanceTask and VmwareUpdateTaskManager

--- cloud-vmware-server/src/main/java/jetbrains/buildServer/clouds/vmware/tasks/VmwareUpdateTaskManager.java
+++ cloud-vmware-server/src/main/java/jetbrains/buildServer/clouds/vmware/tasks/VmwareUpdateTaskManager.java
@@ -1,7 +1,13 @@
 package jetbrains.buildServer.clouds.vmware.tasks;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import jetbrains.buildServer.clouds.base.connector.CloudApiConnector;
 import jetbrains.buildServer.clouds.vmware.VMWareCloudClient;
 import jetbrains.buildServer.clouds.vmware.connector.VMWareApiConnector;
 import org.jetbrains.annotations.NotNull;
@@ -9,33 +15,43 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Created by sergeypak on 27/10/2016.
  */
-public class VmwareUpdateTaskManager implements VmwarePooledUpdateInstanceTask.PooledTaskObsoleteHandler {
+public class VmwareUpdateTaskManager {
 
-  private final ConcurrentHashMap<String, VmwarePooledUpdateInstanceTask> myUpdateTasks;
+  public static final ReadWriteLock POOLED_TASKS_LOCK = new ReentrantReadWriteLock();
+
+  // protected 4 tests
+  protected final Map<String, VmwarePooledUpdateInstanceTask> myUpdateTasks;
 
   public VmwareUpdateTaskManager(){
-    myUpdateTasks = new ConcurrentHashMap<>();
+    myUpdateTasks = new HashMap<>();
   }
 
   @NotNull
-  public synchronized VmwareUpdateInstanceTask createUpdateTask(@NotNull final VMWareApiConnector connector,
-                                                   @NotNull final VMWareCloudClient client){
-    final VmwarePooledUpdateInstanceTask task =
-      myUpdateTasks.computeIfAbsent(connector.getKey(), k -> createNewPooledTask(connector, client));
+  public VmwareUpdateInstanceTask createUpdateTask(@NotNull final VMWareApiConnector connector, @NotNull final VMWareCloudClient client){
+    final Set<String> keys = new HashSet<>();
+    final VmwarePooledUpdateInstanceTask pooledTask;
+    try {
+      POOLED_TASKS_LOCK.writeLock().lock();
+      myUpdateTasks.forEach((key, t) -> {
+        if (t.isExhausted()) {
+          keys.add(connector.getKey());
+        }
+      });
+      keys.forEach(myUpdateTasks::remove);
+      pooledTask = myUpdateTasks.computeIfAbsent(connector.getKey(), k -> createNewPooledTask(connector, client));
+      pooledTask.setShouldKeep();
+    } finally {
+      POOLED_TASKS_LOCK.writeLock().unlock();
+    }
+    final VmwareUpdateInstanceTask task = new VmwareUpdateInstanceTask(connector.getKey(), client, pooledTask);
+    task.register();
 
-    final VmwareUpdateInstanceTask retval = new VmwareUpdateInstanceTask(connector.getKey(), client, task);
-    retval.register();
-
-    return retval;
+    return task;
   }
 
   protected VmwarePooledUpdateInstanceTask createNewPooledTask(@NotNull final VMWareApiConnector connector,
                                                                @NotNull final VMWareCloudClient client){
-    return new VmwarePooledUpdateInstanceTask(connector, client, this);
+    return new VmwarePooledUpdateInstanceTask(connector, client);
   }
 
-  @Override
-  public synchronized void pooledTaskObsolete(@NotNull final VmwarePooledUpdateInstanceTask task) {
-    myUpdateTasks.remove(task.getKey());
-  }
 }
